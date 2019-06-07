648895bd9274f80b83b415f6d146d1a51bd35822
ByeRON <35923134+ByeRON@users.noreply.github.com>
2019-06-07 18:49:32 +0900

    Add files via upload

--- CommonParam.py
+++ CommonParam.py
@@ -1,9 +1,9 @@
 class CommonParam:
 	def __init__(self):
 		self.timeframes = {
-			'5min': 45,
-			'15min': 45
+			'15min': 45,
+			'4H': 45
 		}
 		self.timelist = list(self.timeframes.keys())
-		self.entry_freq = '5min'
-		self.target = '15min'
+		self.entry_freq = '15min'
+		self.target = '4H'
