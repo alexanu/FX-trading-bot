3d513d52a4c67c1ad0fe18bcd890a4f27e3340f9
ByeRON <35923134+ByeRON@users.noreply.github.com>
2020-04-21 15:00:06 +0900

    Add files via upload
    例外処理の冗長な部分を修正

--- bot.py
+++ bot.py
@@ -7,75 +7,12 @@ import pandas as pd
 #User difined classes
 from Manager import Manager
 from CommonParam import CommonParam
-#from OandaEndpoints import Order, Position, Pricing, Instrument
 from OandaEndpoints import Instrument
 from OandaCandleStick import CandleStick
-#from predict_RNN import RNNPredictor
-#from RNNparam import RNNparam
-#from Fetter import Fetter
-#from Plotter import Plotter
-#from Predictor import Predictor
-#from Trader import Trader
-#from Evaluator import Evaluator
-#from PlotHelper import PlotHelper
-#from RoutineInspector import RoutineInspector
-
-#User difined functions
 from Notify import notify_from_line
 from OandaEndpoints import from_byte_to_dict, from_response_to_dict
-from Analysis import is_rise_with_zebratail, is_rise_with_trendline
 
-'''
-Environment            Description
-fxTrade(Live)          The live(real) environment
-fxTrade Practice(Demo) The Demo (virtual) environment
-'''
-
-#def can_update(recv, candlestick, mode=None):
-#	"""
-#	ローソク足が更新可能かを返す, 対象とする時間足を指定する必要はない
-#	for文などで予め対象とするローソク足を抽出する必要あり
-#
-#	Parameters
-#	----------
-#	recv: dict
-#		tickデータを含む受信データ
-#	candlestick: CandleStick
-#		ある時間足のローソク足データ
-#	"""
-#	dummy_tick = pd.Series(float(recv["bids"][0]["price"]),index=[pd.to_datetime(recv["time"])])
-#	dummy_tickdata = candlestick.tickdata.append(dummy_tick)
-#	dummy_ohlc = dummy_tickdata.resample(candlestick.rate).ohlc()
-#
-#	num_candle = candlestick.count if mode is 'init' else 0
-#	
-#	if((num_candle + 2) <= len(dummy_ohlc)):
-#		return True
-#	else:
-#		return False
-#
-#def has_price(msg):
-#	if msg:
-#		msg = from_byte_to_dict(msg)
-#		if msg["type"] == "PRICE":
-#			return True
-#		else:
-#			return False
-#	else:
-#		return False
-#
-#def has_heartbeat(msg):
-#	if msg:
-#		msg = from_byte_to_dict(msg)
-#		if msg["type"] == "HEARTBEAT":
-#			return True
-#		else:
-#			return False
-#	else:
-#		return False
-
-def initialize(timeframes, instrument, environment='demo'):
-	print('Initialize start')
+def initialize(timeframes: dict, instrument: str, environment: str='demo') -> dict:
 	offset_table = {
 		'5S': 'S5',
 		'10S': 'S10',
@@ -107,27 +44,27 @@ def initialize(timeframes, instrument, environment='demo'):
 		'1M': 'M'
 	}
 
-	#timeframesの情報をもとにCandleStickを生成,時間足をキーとして地所型に格納
+	#解析に用いるローソク足を定義
 	candlesticks = {t: CandleStick(t, c) for t, c in timeframes.items()}
-
-	#APIを叩くhandler呼び出し
+	#トレード環境（本番 or 仮想）
 	instrument_handler = Instrument(environment)
 	
-	#任意のすべての時間足において、指定された本数のローソク足を取得
+	#ローソク足データの初期値を設定
 	for k, v in candlesticks.items():
-		#各時間足ごとのローソク足のデータを取得
 		resp = instrument_handler.fetch_candle(instrument, 'M', offset_table[k], v.count)
 		print(resp)
 
-		#接続失敗した場合、Noneを返し終了
-		try:
-			resp.raise_for_status()
-		except ConnectionError as e:
-			print(f'{e}')
-			return None
-		print('Pricing handler get connection')
+		#接続可能かを検証
+		resp.raise_for_status()
+#		try:
+#			resp.raise_for_status()
+#		except ConnectionError as e:
+#			print(f'{e}')
+#			return None
 
-		#取得したローソク足データをdict型へ変換
+		print('Instrument_handler get connection in Initialize from bot.py')
+
+		#取得したローソク足を辞書型へ変換
 		fetched_data = from_response_to_dict(resp)
 
 		time_index = []
@@ -137,6 +74,7 @@ def initialize(timeframes, instrument, environment='demo'):
 			'low': [],
 			'close': []
 		}
+
 		for i in range(v.count):
 			#responseから必要なデータを抽出し、順番にリストに格納する
 			time_index.append(pd.to_datetime(fetched_data['candles'][i]['time']))
@@ -144,36 +82,36 @@ def initialize(timeframes, instrument, environment='demo'):
 			default_ohlc['high'].append(float(fetched_data['candles'][i]['mid']['h']))
 			default_ohlc['low'].append(float(fetched_data['candles'][i]['mid']['l']))
 			default_ohlc['close'].append(float(fetched_data['candles'][i]['mid']['c']))
+
 		#抽出したローソク足データを自作クラスのOHLCに代入
 		ohlc = pd.DataFrame(default_ohlc, index=time_index)
 		located_ohlc = ohlc.loc[:,['open', 'high', 'low', 'close']]
 		v.ohlc = located_ohlc
-		print(v.ohlc)
-		print(len(v.ohlc.index))
-	print('initialize end')
+		print(f'{k}: {len(v.ohlc.index)}')
+
 	return candlesticks
 
 def main():
-	#timeframes = {
-	#	'5min': RNNparam('5min').tau,
-	#	'15min': RNNparam('15min').tau
-	#}
-
 	instrument = 'GBP_JPY'
 	environment = 'demo'
 	mode = 'test'
 	param = CommonParam()
 	manager = Manager(param, instrument, environment, mode)
 
-	#初期化	
-	candlesticks = initialize(param.timeframes, instrument, environment)
-	#APIとの接続が失敗した場合Noneが返り終了
-	if candlesticks is None:
+	#取り扱うローソク足の初期値を設定
+	try:
+		candlesticks = initialize(param.timeframes, instrument, environment)
+	except ConnectionError as e:
+		print(f'{e}')
 		print('failed to connect with Oanda Instrument API')
 		return
 
+	#ローソク足の初期化が心配しているかを検証
+	#if candlesticks is None:
+	#	print('failed to connect with Oanda Instrument API')
+	#	return
+
 	manager.driver(candlesticks)
 
-	print('driver is over')
 if __name__ == "__main__":
 	main()
